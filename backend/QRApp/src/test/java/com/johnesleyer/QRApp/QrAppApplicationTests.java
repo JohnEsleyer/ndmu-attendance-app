289ecc3b293b292fa62c518a2505586a55696d41
john <ralphesleyer@gmail.com>
2023-03-12 18:59:10 +0800

    initialized backend with spring boot

---
+++ backend/QRApp/src/test/java/com/johnesleyer/QRApp/QrAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnesleyer.QRApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
