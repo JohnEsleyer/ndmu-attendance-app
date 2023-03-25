9fe39a3eacf1fb3e5262a913ad05064971d929ef
john <ralphesleyer@gmail.com>
2023-03-26 06:39:50 +0800

    updated ClassAttendanceController

--- backend/QRApp3/src/main/java/com/johnesleyer/QRApp3/Controllers/ClassAttendanceController.java
+++ backend/QRApp3/src/main/java/com/johnesleyer/QRApp3/Controllers/ClassAttendanceController.java
@@ -1,6 +1,12 @@
 package com.johnesleyer.QRApp3.Controllers;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,24 +14,43 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.johnesleyer.QRApp3.Entities.ClassAttendance;
+import com.johnesleyer.QRApp3.Entities.Classroom;
 import com.johnesleyer.QRApp3.Repositories.ClassAttendanceRepository;
 
 
 @RestController
 public class ClassAttendanceController {
-    private final ClassAttendanceRepository ClassAttendanceRepository;
+    private final ClassAttendanceRepository classAttendanceRepository;
 
-    public ClassAttendanceController(ClassAttendanceRepository ClassAttendanceRepository){
-        this.ClassAttendanceRepository = ClassAttendanceRepository;
+    public ClassAttendanceController(ClassAttendanceRepository classAttendanceRepository){
+        this.classAttendanceRepository = classAttendanceRepository;
     }
 
     @PostMapping("/register-classAttendance")
-    public ClassAttendance registerClassAttendance(@RequestBody ClassAttendance ClassAttendance){
-        return ClassAttendanceRepository.save(ClassAttendance);
+    public ClassAttendance registerClassAttendance(@RequestBody ClassAttendance classAttendance){
+        return classAttendanceRepository.save(classAttendance);
     }
 
     @GetMapping("/all-classAttendances")
     public List<ClassAttendance> getAllClassAttendances(){
-        return ClassAttendanceRepository.findAll();
+        return classAttendanceRepository.findAll();
     }
+    
+    @PostMapping("/attendance-by-dateclass")
+    public List<ClassAttendance> getClassAttendanceByDateAndClassroom(@RequestBody Map<String, Object> requestData) {
+        String dateString = (String) requestData.get("date");
+        DateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+        Date date = null;
+        try {
+            date = format.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+        Long classroomId = Long.parseLong(((Map<String, Object>) requestData.get("classroom")).get("id").toString());
+        Classroom classroom = new Classroom();
+        classroom.setId(classroomId);
+        return classAttendanceRepository.findByDateAndClassroom(date, classroom);
+    }
+    
 }
--- backend/QRApp3/src/main/java/com/johnesleyer/QRApp3/Repositories/ClassAttendanceRepository.java
+++ backend/QRApp3/src/main/java/com/johnesleyer/QRApp3/Repositories/ClassAttendanceRepository.java
@@ -1,9 +1,13 @@
 package com.johnesleyer.QRApp3.Repositories;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.johnesleyer.QRApp3.Entities.ClassAttendance;
+import com.johnesleyer.QRApp3.Entities.Classroom;
 
 public interface ClassAttendanceRepository extends JpaRepository<ClassAttendance, Integer>{
-    
+    List<ClassAttendance> findByDateAndClassroom(Date date, Classroom classroom);
 }
