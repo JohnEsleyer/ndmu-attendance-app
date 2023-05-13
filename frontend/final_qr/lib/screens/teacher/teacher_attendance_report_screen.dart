de8e0b4aec69cc5cf388c8c797d00692f8b5da6c
john <ralphesleyer@gmail.com>
2023-05-13 17:41:02 +0800

    updated teacher-attendance_report_screen.dart

--- frontend/final_qr/lib/screens/teacher/teacher_attendance_report_screen.dart
+++ frontend/final_qr/lib/screens/teacher/teacher_attendance_report_screen.dart
@@ -14,7 +14,7 @@ class AttendanceReportScreen extends StatefulWidget {
 }
 
 class _AttendanceReportScreenState extends State<AttendanceReportScreen> {
-  late String _dropdownValue;
+  late String _dropdownValue = '';
   int _indexValue = 0;
   Future<List<dynamic>> fetchStudentsByClass(int classId) async {
     final response = await http.post(
@@ -69,29 +69,33 @@ class _AttendanceReportScreenState extends State<AttendanceReportScreen> {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
-                        DropdownButton<String>(
-                          value: _dropdownValue,
-                          underline: Container(
-                            height: 2,
-                            color: Colors.white,
+                        Flexible(
+                          child: DropdownButton<String>(
+                            dropdownColor: Colors.green,
+                            value: _dropdownValue,
+                            underline: Container(
+                              height: 2,
+                              color: Colors.white,
+                            ),
+                            onChanged: (String? value) {
+                              setState(() {
+                                _dropdownValue = value ?? '';
+                                _indexValue = teacherData.getClassrooms
+                                    .indexWhere((element) => element == value);
+                                print(_dropdownValue);
+                              });
+                            },
+                            items: teacherData.getClassrooms
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(
+                                  value,
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              );
+                            }).toList(),
                           ),
-                          onChanged: (String? value) {
-                            setState(() {
-                              _dropdownValue = value!;
-                              _indexValue = teacherData.getClassrooms
-                                  .indexWhere((element) => element == value);
-                            });
-                          },
-                          items: teacherData.getClassrooms
-                              .map<DropdownMenuItem<String>>((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(
-                                value,
-                                style: TextStyle(color: Colors.white),
-                              ),
-                            );
-                          }).toList(),
                         ),
                       ],
                     ),
@@ -106,93 +110,87 @@ class _AttendanceReportScreenState extends State<AttendanceReportScreen> {
                           ),
                         ),
                         width: MediaQuery.of(context).size.width,
-                        child: Expanded(
-                          child: Center(
-                            child: FutureBuilder<List<dynamic>>(
-                                future: fetchStudentsByClass(
-                                    teacherData.getClassroomID[_indexValue]),
-                                builder: (BuildContext context,
-                                    AsyncSnapshot snapshot) {
-                                  if (!snapshot.hasData) {
-                                    return Container(
-                                      height: 30,
-                                      width: 30,
-                                      child: const CircularProgressIndicator(
-                                        color: Colors.green,
-                                      ),
-                                    );
-                                  } else {
-                                    return Expanded(
-                                      child: ListView.builder(
-                                        physics: BouncingScrollPhysics(),
-                                        itemCount: snapshot.data.length,
-                                        itemBuilder:
-                                            (BuildContext context, int index) {
-                                          // var className =
-                                          //     snapshot.data[index]['className'];
-                                          // var qrURL = snapshot.data[index]['qrURL'];
-                                          // var classId = snapshot.data[index]["id"];
-                                          // var schedule = snapshot.data[index]['schedule'];
-                                          // var defaultTime = snapshot.data[index]['defaultTime'];
-                                          String firstName =
-                                              snapshot.data[index]['student']
-                                                  ["firstName"];
-                                          String lastName = snapshot.data[index]
-                                              ['student']["lastName"];
-                                          return Column(
-                                            children: [
-                                              DelayedWidget(
-                                                child: Container(
-                                                    height: 0.10 *
-                                                        MediaQuery.of(context)
-                                                            .size
-                                                            .height,
-                                                    width: 0.90 *
-                                                        MediaQuery.of(context)
-                                                            .size
-                                                            .width,
-                                                    decoration: BoxDecoration(
-                                                      boxShadow: [
-                                                        BoxShadow(
-                                                          color: Colors.grey
-                                                              .withOpacity(0.5),
-                                                          spreadRadius: 2,
-                                                          blurRadius: 5,
-                                                          offset: Offset(0,
-                                                              3), // changes position of shadow
-                                                        ),
-                                                      ],
-                                                      color: Colors.white,
-                                                      border: Border.all(
-                                                        color: Colors.black,
-                                                        width: 2.0,
-                                                      ),
-                                                      borderRadius:
-                                                          BorderRadius.all(
-                                                        Radius.circular(10),
-                                                      ),
+                        child: Center(
+                          child: FutureBuilder<List<dynamic>>(
+                              future: fetchStudentsByClass(
+                                  teacherData.getClassroomID[_indexValue]),
+                              builder: (BuildContext context,
+                                  AsyncSnapshot snapshot) {
+                                if (!snapshot.hasData) {
+                                  return Container(
+                                    height: 30,
+                                    width: 30,
+                                    child: const CircularProgressIndicator(
+                                      color: Colors.green,
+                                    ),
+                                  );
+                                } else {
+                                  return Expanded(
+                                    child: ListView.builder(
+                                      physics: BouncingScrollPhysics(),
+                                      itemCount: snapshot.data.length,
+                                      itemBuilder:
+                                          (BuildContext context, int index) {
+                                        // var className =
+                                        //     snapshot.data[index]['className'];
+                                        // var qrURL = snapshot.data[index]['qrURL'];
+                                        // var classId = snapshot.data[index]["id"];
+                                        // var schedule = snapshot.data[index]['schedule'];
+                                        // var defaultTime = snapshot.data[index]['defaultTime'];
+                                        String firstName = snapshot.data[index]
+                                            ['student']["firstName"];
+                                        String lastName = snapshot.data[index]
+                                            ['student']["lastName"];
+                                        return Column(
+                                          children: [
+                                            Container(
+                                                height: 0.10 *
+                                                    MediaQuery.of(context)
+                                                        .size
+                                                        .height,
+                                                width: 0.90 *
+                                                    MediaQuery.of(context)
+                                                        .size
+                                                        .width,
+                                                decoration: BoxDecoration(
+                                                  boxShadow: [
+                                                    BoxShadow(
+                                                      color: Colors.grey
+                                                          .withOpacity(0.5),
+                                                      spreadRadius: 2,
+                                                      blurRadius: 5,
+                                                      offset: Offset(0,
+                                                          3), // changes position of shadow
                                                     ),
-                                                    child: Padding(
-                                                      padding:
-                                                          const EdgeInsets.all(
-                                                              8.0),
-                                                      child: Text(
-                                                        "$lastName, $firstName",
-                                                        style: TextStyle(
-                                                          fontSize: 20,
-                                                        ),
-                                                      ),
-                                                    )),
-                                              ),
-                                              SizedBox(height: 10),
-                                            ],
-                                          );
-                                        },
-                                      ),
-                                    );
-                                  }
-                                }),
-                          ),
+                                                  ],
+                                                  color: Colors.white,
+                                                  border: Border.all(
+                                                    color: Colors.black,
+                                                    width: 2.0,
+                                                  ),
+                                                  borderRadius:
+                                                      BorderRadius.all(
+                                                    Radius.circular(10),
+                                                  ),
+                                                ),
+                                                child: Padding(
+                                                  padding:
+                                                      const EdgeInsets.all(8.0),
+                                                  child: Text(
+                                                    "$lastName, $firstName",
+                                                    style: TextStyle(
+                                                      fontSize: 20,
+                                                    ),
+                                                  ),
+                                                )),
+                                            SizedBox(height: 10),
+                                          ],
+                                        );
+                                      },
+                                    ),
+                                  );
+                                }
+                              }),
                         ),
                       ),
                     ),
@@ -201,6 +199,7 @@ class _AttendanceReportScreenState extends State<AttendanceReportScreen> {
               );
             } catch (e) {
               print(e.toString());
+              print("Exception Occured!!");
               return Center(
                 child: Container(
                   child: CircularProgressIndicator(
